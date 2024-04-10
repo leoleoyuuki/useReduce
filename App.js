b439b5a07362319eedd59990527d230e035355b1
leoleoyuuki <rm550373@fiap.com.br>
2024-04-10 11:55:16 -0300

    .

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.tipo) {
+    case "setText":
+      return { ...state, texto: action.payload };
+    case "setNomeCompleto":
+      return { ...state, nomeUsuario: action.payload };
+    default:
+      return state;
+  }
+};
+
+const valorInicial = {
+  texto: "",
+  nomeUsuario: ""
+};
 
 export default function App() {
+  let textoDigitado;
+  let nomeCompleto;
+
+  const [state, dispatch] = useReducer(reducer, valorInicial);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Use Reduce</Text>
+      <Text>{state.texto}</Text>
+      <Text>{state.nomeUsuario}</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={(texto) => (textoDigitado = texto)}
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={(texto) => (nomeCompleto = texto)}
+      />
+
+      <Button
+        title="Alterar Texto"
+        onPress={() => dispatch({ tipo: "setText", payload: textoDigitado })}
+      />
+
+      <Button
+        title="Nome Completo"
+        onPress={() =>
+          dispatch({ tipo: "setNomeCompleto", payload: nomeCompleto })
+        }
+      />
     </View>
   );
 }
@@ -13,8 +58,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 15
+  },
+  input: {
+    width: 300,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 10,
   },
 });
